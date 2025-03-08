71db8d149011880b7cbd2f33065deaccdd4a6db4
Gaurav-rawat-dev <159828861+Gaurav-rawat-dev@users.noreply.github.com>
2025-03-08 17:25:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Load environment variables
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+// Use environment variables
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Middleware to log requests (only in development)
+// if (NODE_ENV === 'development') {
+//   const morgan = require('morgan'); // Use morgan for logging
+//   app.use(morgan('dev'));
+// }else{
+//   console.log("ruing in prodiction mode")
+// }
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send(`Hello, World! Running in ${NODE_ENV} mode.`);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log('Server is running on port 3000');
+});
