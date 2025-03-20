b3a72cec3a64c888d018609eae363c53b83c6fa6
Gaurav-rawat-dev <159828861+Gaurav-rawat-dev@users.noreply.github.com>
2025-03-20 18:18:52 +0530

    added mongo-container

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // Load environment variables
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 const express = require('express');
 const app = express();
+
+
+
+
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+
 
 // Use environment variables
 // const PORT = process.env.PORT || 3000;
