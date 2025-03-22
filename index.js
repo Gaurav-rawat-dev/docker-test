00022b09b479125f6829eb37952e7c9d689bbc1b
Gaurav Rawat <159828861+Gaurav-rawat-dev@users.noreply.github.com>
2025-03-22 15:39:49 +0530

    config.js created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Load environment variables
 const mongoose = require("mongoose");
+const {PORT}  = require("./config/config")
+
 require('dotenv').config();
 
 const express = require('express');
